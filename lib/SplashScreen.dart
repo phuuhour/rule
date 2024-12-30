3d9d571af2253ceccdde48a3e12c2e61b537cdb9
phuuhour <lyvenghour@gmail.com>
2024-12-30 15:07:32 +0700

    add logo

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 Widget SplashScreen() {
   return Scaffold(
-    appBar: AppBar(
-      title: Text('Voice Recognition App'),
-    ),
+    backgroundColor: Colors.white,
     body: Center(
-      child: Text('Welcome to '),
+      child: Image.asset(
+        'assets/imgs/rule_logo.png',
+        height: 160,
+      ),
     ),
   );
 }
