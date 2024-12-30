18f333a50daad1972f6e8cef40fefee8322ffe69
phuuhour <lyvenghour@gmail.com>
2024-12-30 15:42:27 +0700

    add home screen

---
+++ lib/home_all.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Widget HomeAll() {
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: Center(child: Text('HomeAll')),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:school_management_system/SplashScreen.dart';
+import 'package:school_management_system/home_all.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(),
+      home: HomeAll(),
     );
   }
 }
