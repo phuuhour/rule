9e1d56355d33921efcbbf4e1d9e145c2f9ab0806
phuuhour <lyvenghour@gmail.com>
2025-01-11 14:14:05 +0700

    update

--- lib/View/teacher.dart
+++ lib/View/teacher.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class Teacher extends StatefulWidget {
   const Teacher({super.key});
@@ -12,67 +13,101 @@ class _TeacherState extends State<Teacher> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Column(
-            children: [
-              Image(
-                image: AssetImage('assets/imgs/appbar_logo.png'),
-                height: 90,
-              ),
-              const SizedBox(height: 50),
-              Image.asset(
-                'assets/icons/teacher.png',
-                height: 80,
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'Teacher',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              children: [
+                Image(
+                  image: AssetImage('assets/imgs/appbar_logo.png'),
+                  height: 90,
                 ),
-              ),
-              const Text(
-                'Login to your account',
-                style: TextStyle(
-                  fontSize: 14,
+                const SizedBox(height: 50),
+                Image.asset(
+                  'assets/icons/teacher.png',
+                  height: 80,
                 ),
-              ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Teacher ID',
-                    suffixIcon: Icon(
-                      Icons.verified_user,
-                      color: Colors.amber,
-                    ),
-                    prefixIcon: Icon(Icons.person, color: Colors.amber),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                      ),
-                    ),
+                const SizedBox(height: 15),
+                const Text(
+                  'Teacher',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                const Text(
+                  'Login to your account',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Teacher ID',
+                        prefixIcon: Icon(HugeIcons.strokeRoundedUserCircle,
+                            color: Colors.amber),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: BorderSide(
+                            color: Colors.amber,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: BorderSide(
+                            color: Colors.amber,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: BorderSide(
+                            color: Colors.amber,
+                            width: 2,
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white70),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        suffixIcon: Icon(
+                          HugeIcons.strokeRoundedViewOffSlash,
+                          color: Colors.amber,
+                        ),
+                        prefixIcon: Icon(HugeIcons.strokeRoundedLockPassword,
+                            color: Colors.amber),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: BorderSide(
+                            color: Colors.amber,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: BorderSide(
+                            color: Colors.amber,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                          borderSide: BorderSide(color: Colors.amber, width: 2),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white70),
+                    obscureText: true,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
